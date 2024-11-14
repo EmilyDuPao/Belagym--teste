634c6439a7c35f14069c219f911c24b7f0198887
Emily da Conceição Lima <EMILYDACONCEICAOLIMAPAO@GMAIL.COM>
2024-11-14 09:51:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
-// nav background
-let header = document.querySelector("header");
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-window.addEventListener("scroll", () => {
-    header.classList.toggle("shadow", window.scrollY > 0)
-})
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
 
-//Filter
-$(document).ready(function () {
-    $(".filter-item").click(function () {
-        const value = $(this).attr("data-filter");
-        if (value == "all"){
-            $(".post-box").show("1000")
-        } else{
-            $(".post-box")
-                .not("." + value)
-                .hide(1000);
-            $(".post-box")
-            .filter("." + value)
-            .show("1000")
-        }
-    });
-    $(".filter-item").click(function () {
-        $(this).addClass("active-filter").siblings().removeClass("active-filter")
-    });
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".about__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".trainer__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".blog__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+window.onload = function() {
+  document.getElementById('popup-ad').style.display = 'block';
+};
+
+function closeAd() {
+  document.getElementById('popup-ad').style.display = 'none';
+}
